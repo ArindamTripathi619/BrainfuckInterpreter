c0d10b08505274292fd28997f13673074ede6a99
Arindam Tripathi <118624002+ArindamTripathi619@users.noreply.github.com>
2023-01-05 09:28:01 +0530

    Update GifOptionPane.java

--- src/GifOptionPane.java
+++ src/GifOptionPane.java
@@ -2,9 +2,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 public class GifOptionPane extends Interpret {
-    static ImageIcon icon = new ImageIcon("th-3.jpg");
+    static ImageIcon icon = new ImageIcon("src/th-3.jpg");
     static void waitAnimation(int delaySec){
-        String gifPath = "Settings.gif";
+        String gifPath = "src/Settings.gif";
         JLabel label = new JLabel(new ImageIcon(gifPath));
         JOptionPane pane = new JOptionPane(label, JOptionPane.PLAIN_MESSAGE, JOptionPane.DEFAULT_OPTION, icon, new Object[]{}, null);
         JDialog dialog = pane.createDialog(null, "Loading...");
